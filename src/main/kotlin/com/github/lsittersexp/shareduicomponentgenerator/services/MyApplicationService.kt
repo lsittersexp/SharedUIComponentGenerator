4a2f9b52739eb934af31fa80744fe95f7e28721c
GitHub Action <action@github.com>
2022-02-07 15:40:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lsittersexp/shareduicomponentgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lsittersexp.shareduicomponentgenerator.services
+
+import com.github.lsittersexp.shareduicomponentgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
